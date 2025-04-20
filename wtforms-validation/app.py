34e2bdea14e7f36069403e538de442ba6828e78b
CtanOfTheAbyss <sjpervel@yahoo.com>
2025-04-20 14:46:31 -0400

    lol

--- wtforms-validation/app.py
+++ wtforms-validation/app.py
@@ -1,30 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = "mi_clave_secreta"
 
 class LoginForm(FlaskForm):
-    username = StringField("Nombre de Usuario",
-    validators=[DataRequired(), Length(min=3)])
-    password = PasswordField("Contraseña",
-    validators=[DataRequired()])
+    username = StringField("Nombre de Usuario", validators=[DataRequired(), Length(min=3)])
+    password = PasswordField("Contraseña", validators=[DataRequired()])
     submit = SubmitField("Iniciar Sesión")
-
 
 @app.route("/", methods=["GET"])
 def index():
-    login()
+    return redirect(url_for("login"))  # Redirecciona correctamente a /login
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm() # Definir las reglas de validación
-    if form.validate_on_submit(): # Validar los datos entrados contra las reglas
+    form = LoginForm()
+    if form.validate_on_submit():
         return f"Usuario: {form.username.data}"
-    
     return render_template("index.html", form=form)
 
 if __name__ == "__main__":
