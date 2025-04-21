37c4f1f82ba33b2eb1041689fb667723a4659b6d
CtanOfTheAbyss <sjpervel@yahoo.com>
2025-04-20 23:55:52 -0400

    l

--- Actividades/mod2/lec02/app.py
+++ Actividades/mod2/lec02/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, redirect, flash
+from forms import RegistroForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "clave_segura_123"
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Si leíste esto es porque funciona!"
-
-@app.route("/mensaje", methods=["POST"])
-def saludo():
-    data = request.get_json()
-    nombre = data.get("nombre", "Usuario")
-    return f"Hola, {nombre}!"
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    form = RegistroForm()
+    if form.validate_on_submit():
+        flash("Usuario registrado correctamente.", "success")
+        return redirect("/registro")
+    return render_template("register.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
